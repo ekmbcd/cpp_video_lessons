06974ed21f3f2b618061676166c07a153f721079
Andrea De felice <ade-feli@e3r9p3.42roma.it>
2021-05-12 11:14:53 +0200

    day 7

--- 36-Templates/main2.cpp
+++ 36-Templates/main2.cpp
@@ -12,3 +12,20 @@ public:
 	List<T>(T const & content);
 	~List();
 };
+
+// we can use multiple typenames in the same template
+template<typename T, typename U>
+class Test
+{
+};
+
+int main()
+{
+	// T can be int, char... or even another class
+	List< int > a(42);
+	// we have function parameters and type perameters
+	List< float > b(4.2);
+	List< List<int> > c(a);
+
+	Test<char, double> t;
+}
